07b2a7bfd8b7d36c6d2956ab4b52ae6e8bc8bf98
Amir PashaZadeh <amirpa@systemgroup.net>
2025-08-13 11:14:29 +0330

    register and login pages are okey

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './pages/home2/home2.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+];
