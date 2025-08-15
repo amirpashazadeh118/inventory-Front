1d9ad6c80f37202e157801bf0ba1cd577f6299f4
Amir PashaZadeh <amirpa@systemgroup.net>
2025-08-15 16:51:16 +0330

    ADD: history done

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterModule, HttpClientModule, CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  sells: any[] = [];
+  buys: any[] = [];
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    const params = new HttpParams()
+      .set('state', '2');
+
+    this.http.get<any[]>('/api/inventory/Order', { params }).subscribe({
+      next: (res) => {
+        this.sells = res;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+
+    this.http.get<any[]>('/api/inventory/InventoryVoucher').subscribe({
+      next: (res) => {
+        this.buys = res;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 }
